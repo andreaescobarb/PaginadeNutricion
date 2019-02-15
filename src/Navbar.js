4360d3b3460bc2702590aabb1537c6775347579a
Andrea <andreajaninaescobar@gmail.com>
2019-02-15 17:39:34 -0600

    Navigation Bar done :)

---
+++ src/Navbar.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import logonutricion from './logonutricion.PNG';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
+} from 'reactstrap';
+
+export default class Example extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+    render() {
+        return (
+            <div>
+                <Navbar color="fixed" light expand="md">
+                    <NavbarBrand href="/"><img src={logonutricion} height="100" alt="logonutricion"/></NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <NavLink href="/components/">Components</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+                            </NavItem>
+                            <UncontrolledDropdown nav inNavbar>
+                                <DropdownToggle nav caret>
+                                    Options
+                </DropdownToggle>
+                                <DropdownMenu right>
+                                    <DropdownItem>
+                                        Option 1
+                  </DropdownItem>
+                                    <DropdownItem>
+                                        Option 2
+                  </DropdownItem>
+                                    <DropdownItem divider />
+                                    <DropdownItem>
+                                        Reset
+                  </DropdownItem>
+                                </DropdownMenu>
+                            </UncontrolledDropdown>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </div>
+        );
+    }
+}
+
+/*Navbar.propTypes = {
+    light: PropTypes.bool,
+    dark: PropTypes.bool,
+    fixed: PropTypes.string,
+    color: PropTypes.string,
+    role: PropTypes.string,
+    expand: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
+    tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string])
+    // pass in custom element to use
+}
+
+NavbarBrand.propTypes = {
+    tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string])
+    // pass in custom element to use
+}*/
