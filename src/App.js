d2a3c2099f1ba138481929b29e2881f55343f0da
Andrea <andreajaninaescobar@gmail.com>
2019-02-16 17:15:54 -0600

    Carousel con fotos lindas done

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import Navbar from "./Navbar.js";
 import './App.css';
-
+import Carousel from './Carousel.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar></Navbar>
+        <Carousel></Carousel>
       </div>
     );
   }
