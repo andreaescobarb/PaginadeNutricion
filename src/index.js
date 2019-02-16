0bc0a941c1dc02afb6fc70918de705bca43c5be8
Andrea <andreajaninaescobar@gmail.com>
2019-02-15 19:22:04 -0600

    favicon

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Favicon from 'react-favicon';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App /> ,
+    <Favicon url="./favicon.png" />, 
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
